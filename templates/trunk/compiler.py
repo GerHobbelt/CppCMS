a0192e92dd7b85b1414f744dd2b47b304fbc5460
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2008-03-18 20:48:25 +0000

    Compiler progress

--- templates/trunk/compiler.py
+++ templates/trunk/compiler.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import sys
 
 class tocken:
 	def __init__(self,name,param):
@@ -17,40 +18,124 @@ def interleave(*args):
 			except IndexError:
 				continue
 
-class callback:
-	def __init__(self,pattern,call):
-		self.pattern=pattern
-		self.call=call
 
-def template(name):
-	print "# template %s" % name
-	print "export %s" % name
+class template:
+	pattern=r'^<%\s*template\s+([a-zA-Z]\w*)\s*%>$'
+	def use(self,m):
+		name=m.group(1)
+		print "# template %s" % name
+		print "export %s" % name
 
-def inline_content(content):
-	if content=='' :
-		return
-	print "inline '%s'" % content[1:5]
+class inline_content:
+	pattern=r'(.*)';
+	def use(self,m):
+		content=m.group(0)
+		print "\tinline '%s'" % to_string(content)
 
 def error_exit(x):
+	global exit_flag
 	print "Error %s" % x
-#	exit(1)
+	exit_flag=1
 
-def ifop(param,name):
-	print "\tif %s\t%s" % (param , name)
+def new_label():
+	global labels_counter
+	s="LBL_%d" % labels_counter
+	labels_counter+=1
+	return s
 
-def prepare(calls):
-	list =[ [ r'^<%\s*template\s+([a-zA-Z]\w*)\s*%>$' , (lambda x: template(x.group(1))) ], \
-		[ r'^<%\s*(elif|if)(\s+(not|\s+)([a-zA-Z]\w*)\s*%>$' , (lambda x : ifop('true',x)) ],\
-		[ r'^<%\s*(elif|if)\s+not\s+([a-zA-Z]\w*)\s*%>$' , (lambda x : ifop('false',x)) ],\
-		[ r'^<%\s*(if\s+def\s+([a-zA-Z]\w*)\s*%>$' , (lambda x : ifop('def',x)) ],\
-		[ r'^<%\s*if\s+not\s+def\s+([a-zA-Z]\w*)\s*%>$' , (lambda x : ifop('ndef',x)) ],\
-		[ r'^<%\s*if\s+not\s+def\s+([a-zA-Z]\w*)\s*%>$' , (lambda x : ifop('ndef',x)) ],\
-		[ r'^<%(.*)%>$',(lambda s: error_exit("Unkown command '%s'"%s)) ], \
-		[ r'^(.*)$',inline_content] ]
-	for a in list:
-		calls.append(callback(re.compile(a[0]),a[1]))
+def to_string(s):
+	res=''
+	for ch in s:
+		if ch < ' ':
+			res+=("\\x%02x" % ch)
+		else:
+			res+=ch
+	return res
+
+
+def sequence(seq_name):
+	if tmpl_seq.has_key(seq_name):
+		return tmpl_seq[seq_name]
+	error_exit("Undefined sequence %s" % seq_name)
+	# FIIIX MMMEEE
+	return 0
+
+def make_ident(val):
+	m=re.match(r'^ARG_(\d)$',val)
+	if m :
+		return "%s" % (template_id*10+int(m.group(1)))
+	m=re.match(r'^\w+$',val)
+	if m:
+		return val
+	m=re.match(r'^(\w+)\.(\w+)$',val)
+	seq_id=sequence(m.group(1))
+	return "%s(%d)" % ( m.group(2),seq_id )
+
+class foreach_block:
+	pattern=r'^<%\s*foreach\s+in\s+([a-zA-Z]\w*(\.([a-zA-Z]\w*))?)\s*%>$'
+	def use(self):
+		if tmpl_seq.has_key(seq_name):
+			error_exit("Nested sequences with same name")
+		pass
+	def on_end(self):
+		pass
+
+class ifop:
+	pattern=r'^<%\s*(if|elif)\s+((not)\s+)?((def)\s+)?([a-zA-Z]\w*(\.([a-zA-Z]\w*))?)\s*%>$'
+	type='if'
+	def prepare(self):
+		ident_str=make_ident(self.ident)
+		self.label=new_label()
+		if self.has_def:
+			print "\tdef\t%s" % ident_str
+		else:
+			print "\ttrue\t%s" % ident_str
+		if self.has_not:
+			print "\tjmp\tt,%s" % self.label
+		else:
+			print "\tjmp\tf,%s" % self.label
+
+	def on_end(self):
+		print "%s:" % self.label
+
+	def use(self,m):
+		global stack
+		self.type=m.group(1)
+		self.has_not=m.group(3)
+		self.has_def=m.group(5)
+		self.ident=m.group(6)
+		if self.type == 'if' :
+			stack.append(self)
+			self.prepare()
+		else: # type == elif
+			if len(stack)!=0 :
+				prev=stack.pop()
+				if prev.type!='if':
+					error_exit("elif without if");
+				prev.on_end()
+				stack.append(self)
+				self.prepare()
+			else:
+				error_exit("Unexpeced elif");
+# END ifop				
+			
+
+class end_block:
+	pattern=r'^<%\s*end\s*%>';
+	def use(self,m):
+		global stack
+		if len(stack)==0:
+			error_exit("Unexpeced 'end'");
+		else:
+			stack.pop().on_end()
+
+class error_com:
+	pattern=r'^<%(.*)%>$'
+	def use(self,m):
+		error_exit("unknown command %s" % m.group(1))
 
 def main():
+	global stack
 	for file in os.sys.argv[1:]:
 		f=open(file,'r')
 		content=f.read()
@@ -58,17 +143,20 @@ def main():
 		texts=re.split(r'<\%[^\%]*\%\>',content)
 		commands=re.findall(r'<\%[^\%]*\%\>',content)
 		for x in interleave(texts,commands):
-			for c in calls:
-				m = c.pattern.match(x)
+			if x=='' : continue
+			for c in [  ifop(), template(), end_block(), error_com() , inline_content()]:
+				m = re.match(c.pattern,x)
 				if m :
-					c.call(m.group(1))
+					c.use(m)
+					break
 
 		if(len(stack)!=0):
 			sys.stderr.write("Unexpected end of file %s\n" % file)
 			exit(1)
 
+labels_counter=0
+tmpl_seq={}
+seq_no=0
 stack=[]
-calls=[]
-prepare(calls);
+exit_flag=0
 main()
-
