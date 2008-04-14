a9bbf63a3449678907a71d04afd9ae2ffb46155d
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2008-04-14 19:41:16 +0000

    The code supports new templates with correct autoconf configuration

--- cms/trunk/templates/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if tmpl_bld look.opcode admin_editpost.tmpl admin_login.tmpl admin_main.tmpl error.tmpl feed_posts.tmpl main_page.tmpl post.tmpl sidebar.tmpl master.tmpl admin.tmpl ; then
-	echo Complete
-	exit 0
-else
-	echo Failed
-	exit 0
-fi
