1d493c69536f6f21f8f881ab3ee6f0ac4c17c77b
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2008-10-18 21:05:55 +0000

    Updated for new template system

--- examples/trunk/hello_world_templates/hello-tmpl.cpp
+++ examples/trunk/hello_world_templates/hello-tmpl.cpp
@@ -1,39 +1,34 @@
 #include <cppcms/worker_thread.h>
 #include <cppcms/manager.h>
-#include <tmpl/content.h>
-#include <tmpl/renderer.h>
-#include <iostream>
+
+#include "data.h"
+
 using namespace std;
 using namespace cppcms;
-using namespace tmpl;
 
 class my_hello_world : public worker_thread {
-    renderer rnd;
 public:
-    my_hello_world(manager const &s,template_data const &t) :
-        worker_thread(s),
-	rnd(t)
+    my_hello_world(manager const &s) :
+        worker_thread(s)
     {
+    	use_template("my_view");
     };
     virtual void main();
 };
 
 void my_hello_world::main()
 {
-    content c;
-    c["message"]=string("Hello");
-    rnd.render(c,"main",out);
+    data::message c;
+    c.message="Hello";
+    render("message",c);
 }
 
 int main(int argc,char ** argv)
 {
-    template_data templ;
     try {
         manager app(argc,argv);
-        templ.load(app.config.sval("templates.file"));
-	app.set_worker(
-	    new one_param_factory<my_hello_world,template_data>(templ));
-	app.execute();
+        app.set_worker(new simple_factory<my_hello_world>());
+        app.execute();
     }
     catch(std::exception const &e) {
         cerr<<e.what()<<endl;
