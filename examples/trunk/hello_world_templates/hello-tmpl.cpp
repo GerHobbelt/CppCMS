84e71b064b287b81f972e947b632742d517837dc
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2008-11-10 14:38:29 +0000

    Updated according to new API

--- examples/trunk/hello_world_templates/hello-tmpl.cpp
+++ examples/trunk/hello_world_templates/hello-tmpl.cpp
@@ -1,4 +1,4 @@
-#include <cppcms/worker_thread.h>
+#include <cppcms/application.h>
 #include <cppcms/manager.h>
 
 #include "data.h"
@@ -6,10 +6,10 @@
 using namespace std;
 using namespace cppcms;
 
-class my_hello_world : public worker_thread {
+class my_hello_world : public application {
 public:
-    my_hello_world(manager const &s) :
-        worker_thread(s)
+    my_hello_world(worker_thread &s) :
+       application(s)
     {
     	use_template("my_view");
     };
@@ -19,7 +19,7 @@ public:
 void my_hello_world::main()
 {
     data::message c;
-    c.message="Hello";
+    c.message=">>>Hello<<<";
     render("message",c);
 }
 
@@ -27,7 +27,7 @@ int main(int argc,char ** argv)
 {
     try {
         manager app(argc,argv);
-        app.set_worker(new simple_factory<my_hello_world>());
+        app.set_worker(new application_factory<my_hello_world>());
         app.execute();
     }
     catch(std::exception const &e) {
