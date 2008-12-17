7b044a7b3d0113a233e8ed01afbbf1b764806e01
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2008-12-17 20:17:45 +0000

    - Sqlite Fix --- no tabs to make libdbi happy - Updates of build scripts

--- examples/trunk/mb/Makefile.spec
+++ examples/trunk/mb/Makefile.spec
@@ -1,7 +1,23 @@
 CXXFLAGS=-Wall -g -O0 -I../inc
-LIBS=-lcppcms -ldbixx
+
+EXEC_CXXFLAGS=$(CXXFLAGS)
+EXEC_LDFLAGS=-export-dynamic
+EXEC_LIBS=-lcppcms -ldbixx
+
+VIEW_CXXFLAGS=$(CXXFLAGS) -fPIC -DPIC
+VIEW_LDFLAGS=-shared
+VUEW_LIBS=-lcppcms
+
 CXX=g++
 CTMPL=cppcms_tmpl_cc
 GETTEXT_DOMAIN=mb
+
+# Linux
+SO_EXT=so
+EXEC_EXT=
+
+# cygwin
+# SO_EXT=dll
+# EXEC_EXT=.exe
 
 
--- examples/trunk/mb/sqlite3.sql
+++ examples/trunk/mb/sqlite3.sql
@@ -3,16 +3,16 @@ drop table if exists threads;
 drop table if exists messages;
 
 create table threads (
-	id integer primary key autoincrement not null,
-	title varchar(256) not null
+    id integer primary key autoincrement not null,
+    title varchar(256) not null
 );
 
 create table messages (
-	id integer primary key autoincrement not null,
-	reply_to integer not null,
-	thread_id integer not null,
-	author varchar(256) not null,
-	content text not null
+    id integer primary key autoincrement not null,
+    reply_to integer not null,
+    thread_id integer not null,
+    author varchar(256) not null,
+    content text not null
 );
 
 commit;
